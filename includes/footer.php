e14ab03bf5f7bb5302b5a30b53a13fd7133485dc
Robin BIDOT <rbidot@hotmail.fr>
2025-04-17 09:52:52 +0200

    maj du header et footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <footer class="text-center mt-5 mb-3 text-muted">
     <p>&copy; <?= date('Y') ?> To-Do App</p>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -12,14 +12,31 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">To-Do App</a>
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ms-auto">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php if (isset($_SESSION['user_id'])): ?>
-                    <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">🧾 Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="documents.php">📁 Documents</a>
+                    </li>
                     <?php if ($_SESSION['role'] === 'admin'): ?>
-                        <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin_dashboard.php">⚙️ Admin</a>
+                        </li>
                     <?php endif; ?>
-                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">🔓 Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Login</a>
+                    </li>
                 <?php endif; ?>
             </ul>
         </div>
